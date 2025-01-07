a9f721d8973f58b365e922671f39e03f763b3326
Odizinne <flora.aubry@proton.me>
2025-01-07 14:52:55 +0100

    cleanup again

--- main.cpp
+++ main.cpp
@@ -1,23 +1,11 @@
 #include <QGuiApplication>
-#include <MainWindow.h>
 #include <QIcon>
-#include <QLocale>
-#include <QTranslator>
-#include <QDir>
+#include "MainWindow.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
     QGuiApplication::setWindowIcon(QIcon(":/icons/icon.png"));
-
-    //QLocale locale;
-    //QString languageCode = locale.name().section('_', 0, 0);
-    //QTranslator translator;
-    //if (translator.load(":/translations/Retr0Mine_" + languageCode + ".qm")) {
-    //    app.installTranslator(&translator);
-    //}
-
     MainWindow w;
-
     return app.exec();
 }
