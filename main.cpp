e841344d84fcb9cf5a8a91243bdb9a9b2e356a81
Odizinne <flora.aubry@proton.me>
2025-02-16 13:25:01 +0100

    disable qtmultimedia logging

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
+#include "MainWindow.h"
 #include <QGuiApplication>
 #include <QIcon>
-#include "MainWindow.h"
+#include <QLoggingCategory>
 
 int main(int argc, char *argv[])
 {
+    QLoggingCategory::setFilterRules("qt.multimedia.ffmpeg=false");
     QGuiApplication app(argc, argv);
     app.setOrganizationName("Odizinne");
     app.setApplicationName("Retr0Mine");
